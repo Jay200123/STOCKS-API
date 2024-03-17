c1455883fcf0e8e6129f2e7e57dde437a58e0cde
Jay200123 <kenken.ayonon@gmail.com>
2024-03-18 00:38:18 +0800

    add transactionController

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,11 +1,17 @@
 const transactionService = require("../services/transactionService");
-const ErrorHandler = require("../utils/errorHandler");
 const SuccessHandler = require("../utils/successHandler");
 const asyncHandler = require("express-async-handler");
+
+exports.getAllTransactions = asyncHandler(async (req, res, next) => {
+    const transactions = await transactionService.getAllTransactionData();
+    return !transactions
+      ? next(new ErrorHandler("transactions not Found"))
+      : SuccessHandler(res, `transactions Found`, transactions);
+  });
 
 exports.createTransaction = [
     asyncHandler((req, res, next)=>{
         const transaction = transactionService.createTransactionData(req);
-        return SuccessHandler(res, `Transaction Successfully Updated`, transaction);
+        return SuccessHandler(res, `Transaction Successfully Created`, transaction);
     })
 ];
