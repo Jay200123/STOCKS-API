159f075ff9c32001e0ed3eba5d8150d8c93aa113
Jay200123 <kenken.ayonon@gmail.com>
2024-03-18 00:38:03 +0800

    add product model

--- models/product.js
+++ models/product.js
@@ -2,31 +2,36 @@ const mongoose = require("mongoose");
 const { RESOURCE } = require("../constants/index");
 
 const productSchema = new mongoose.Schema({
-    product_name:{
+    product_name: {
         type: String,
         required: [true, "Product name field required"],
         maxLength: [60, "Product name must not exceed to 60 characters"],
     },
-    price:{
+    price: {
         type: Number,
         required: [true, "Price field required"],
         default: 0,
     },
-    quantity:{
+    quantity: {
         type: Number,
         required: [true, "Quantity field required"],
         default: 0,
     },
-    product_session:{
+    product_session: {
         type: Number,
         required: [true, "Please Enter product number per session"],
         default: 0,
     },
-    product_volume:{
+    product_volume: {
         type: Number,
         required:[true, "Please enter product volume"],
         default: 0,
-    }
+    },
+    current_volume: {
+        type: Number,
+        default: 0,
+    },
 });
+
 
 module.exports = mongoose.model(RESOURCE.PRODUCT, productSchema);
