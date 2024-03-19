f995ce2fecf17d69d65e2e06c8297675c1bd4325
Jay200123 <kenken.ayonon@gmail.com>
2024-03-20 00:46:58 +0800

    Add test script

--- test.js
+++ test.js
@@ -14,3 +14,37 @@ const currentValue = 1500;
 
 const newValue = currentValue >= 1000 ? `${currentValue / 1000} Liter` : `${currentValue} ml`;
 console.log(newValue);
+
+testValue = "Hair"
+desc1 = "Short Hair"
+// desc2 = "Short Hair";
+
+// eto ung service type 
+if (testValue === "Hair") {
+    try{
+        //dito ko kukunin user description
+        if(desc1 === "Long Hair"){
+            let product_vol = 200;
+            console.log(product_vol);
+        } else if (desc1 === "Short Hair"){
+            product_vol = 100;
+            console.log(product_vol);
+        }
+    }catch(err){
+        console.log(err)
+    }
+}else{
+    console.log(test)
+}
+
+// const productVolume = 1000;
+
+// const sessionVolume = 200 / 1000 * 1000;
+
+// let deduction = sessionVolume - 150;
+// console.log(deduction);
+
+// console.log(sessionVolume);
+
+const tst  = 100 * 0.2 * 10;
+console.log(tst);
