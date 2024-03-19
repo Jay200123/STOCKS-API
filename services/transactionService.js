ecea2c216c3e238b7f15fe272cf7453b24328820
Jay200123 <kenken.ayonon@gmail.com>
2024-03-20 00:44:30 +0800

    Add user description logic

--- services/transactionService.js
+++ services/transactionService.js
@@ -2,6 +2,7 @@ const Transaction = require("../models/transaction");
 const Service = require("../models/service");
 const Product = require("../models/product");
 const Inventory = require("../models/inventory");
+const User = require("../models/user");
 const ErrorHandler = require("../utils/errorHandler");
 
 exports.getAllTransactionData = async () => {
@@ -20,6 +21,24 @@ exports.createTransactionData = async (req, res) => {
 
   try {
     const serviceIds = transaction.service;
+
+    // const data = await Transaction.findById(transaction?._id)
+    // .populate({
+    //   path:"user",
+    //   select: "name description"
+    // })
+    // .collation({ locale:"en"})
+    // .lean()
+    // .exec()
+
+    // const description = user?.user?.description
+    // console.log(description);
+
+    // if(description?.includes("Long Hair")){
+    //   console.log("Shabu pa boss")
+    // }else if(description?.includes("Short Hair")){
+    //   console.log("Dora ka boss")
+    // }
 
     const serviceCount = await Service.find({ _id: { $in: serviceIds } })
       .lean()
@@ -44,11 +63,36 @@ exports.createTransactionData = async (req, res) => {
 
       for (const product of service.product) {
         const newVolume = product.current_volume - product.product_session;
+        let vol_desc; // Define vol_desc in a scope that covers both if blocks
+
+        if (service?.type?.includes("Hair")) {
+          const transacId = transaction?._id;
+          const customer = await Transaction.findById(transacId)
+            .populate({
+              path: "user",
+              select: "name description",
+            })
+            .collation({ locale: "en" })
+            .lean()
+            .exec();
+
+          const description = customer?.user?.description;
+
+          if (description?.includes("Long Hair")) {
+            vol_desc =
+              product.current_volume * 0.2 * 10 - product.product_session;
+            console.log(vol_desc);
+          }
+        }
+
+        const volume_val = service?.type?.includes("Hair")
+          ? vol_desc
+          : newVolume;
 
         const productStock = await Product.findByIdAndUpdate(
           product._id,
           {
-            current_volume: newVolume,
+            current_volume: volume_val,
           },
           {
             new: true,
@@ -61,10 +105,10 @@ exports.createTransactionData = async (req, res) => {
           await productStock.save();
         }
 
-        if(productStock.current_volume < 1000){
-          productStock.measurement = "ml"
-        }else{
-          productStock.measurement = "Liter"
+        if (productStock.current_volume < 1000) {
+          productStock.measurement = "ml";
+        } else {
+          productStock.measurement = "Liter";
         }
         await productStock.save();
 
