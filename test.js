76f67df8c44e9273b6d863db80755e5d55f2769f
Jay200123 <kenken.ayonon@gmail.com>
2024-03-20 16:27:46 +0800

    Add percentage formula

--- test.js
+++ test.js
@@ -20,22 +20,22 @@ desc1 = "Short Hair"
 // desc2 = "Short Hair";
 
 // eto ung service type 
-if (testValue === "Hair") {
-    try{
-        //dito ko kukunin user description
-        if(desc1 === "Long Hair"){
-            let product_vol = 200;
-            console.log(product_vol);
-        } else if (desc1 === "Short Hair"){
-            product_vol = 100;
-            console.log(product_vol);
-        }
-    }catch(err){
-        console.log(err)
-    }
-}else{
-    console.log(test)
-}
+// if (testValue === "Hair") {
+//     try{
+//         //dito ko kukunin user description
+//         if(desc1 === "Long Hair"){
+//             let product_vol = 200;
+//             console.log(product_vol);
+//         } else if (desc1 === "Short Hair"){
+//             product_vol = 100;
+//             console.log(product_vol);
+//         }
+//     }catch(err){
+//         console.log(err)
+//     }
+// }else{
+//     console.log(test)
+// }
 
 // const productVolume = 1000;
 
@@ -46,5 +46,17 @@ if (testValue === "Hair") {
 
 // console.log(sessionVolume);
 
-const tst  = 100 * 0.2 * 10;
-console.log(tst);
+// const tst = 100 * 0.5 / 10 * 10;
+// console.log(tst);
+
+let product_vol = 1000;
+
+let long_vol = product_vol * 0.5 / 10;
+console.log(long_vol);
+let current_volume = 700;
+// console.log(long_vol)
+let newVolume = current_volume - long_vol;
+console.log(newVolume);
+
+const test2 = 100 * 0.5 / 10;
+console.log("Test mo ulo mo", test2);
