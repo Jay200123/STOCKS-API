2b6aaa52a6818b17dbd39dc47b628658d091c261
Jay200123 <kenken.ayonon@gmail.com>
2024-04-06 23:00:54 +0800

    Add logbook equipment routes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 require("dotenv").config({
-    path:"./config/.env"
+  path: "./config/.env",
 });
 
 const Delivery = require("./models/delivery");
@@ -18,46 +18,51 @@ const services = require("./routes/service");
 const transaction = require("./routes/transaction");
 const inventory = require("./routes/inventory");
 const user = require("./routes/user");
+const equipment = require("./routes/equipment");
+const logbook = require("./routes/logbook");
 
 const connectDB = require("./config/connect");
 connectDB();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }))
+app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  "/api/v1",
+  products,
+  delivery,
+  services,
+  transaction,
+  inventory,
+  user,
+  equipment,
+  logbook,
+);
 
-app.use("/api/v1",
- products,
- delivery,
- services,
- transaction,
- inventory,
- user,
+app.get("/", (req, res) => {
+  data = { message: "Testing backend data!" };
+  res.status(STATUSCODE.SUCCESS).json(data);
+});
+
+app.get("/truncate", async (req, res) => {
+  try {
+    // await Delivery.deleteMany({});
+    // await Product.deleteMany({});
+    // await Service.deleteMany({});
+    await Inventory.deleteMany({});
+    await Transaction.deleteMany({});
+
+    const message = "All collections truncated successfully";
+    res.status(STATUSCODE.SUCCESS).json({ message });
+  } catch (error) {
+    console.error("Error truncating collections:", error);
+    const message = "Error truncating collections";
+    res.status(STATUSCODE.SERVER_ERROR).json({ message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server Running on PORT ${process.env.PORT} on ${process.env.NODE_ENV}`
   );
-
-app.get("/", (req, res)=>{
-    data = { message:"Testing backend data!" }
-    res.status(STATUSCODE.SUCCESS).json(data);
-})
-
-app.get('/truncate', async (req, res) => {
-    try {
-        // await Delivery.deleteMany({});
-        // await Product.deleteMany({});
-        // await Service.deleteMany({});
-        await Inventory.deleteMany({});
-        await Transaction.deleteMany({});
-
-      const message = 'All collections truncated successfully';
-      res.status(STATUSCODE.SUCCESS).json({ message });
-    } catch (error) {
-      console.error('Error truncating collections:', error);
-      const message = 'Error truncating collections';
-      res.status(STATUSCODE.SERVER_ERROR).json({ message });
-    }
-  });
-
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server Running on PORT ${process.env.PORT} on ${process.env.NODE_ENV}`);
-})
+});
