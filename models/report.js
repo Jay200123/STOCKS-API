21e406f85fc16289ba767c46f0bdd7614df41973
Jay200123 <kenken.ayonon@gmail.com>
2024-04-10 23:13:29 +0800

    Add report model

---
+++ models/report.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const { RESOURCE } = require("../constants/index");
+
+const reportSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "User required"],
+    ref: RESOURCE.USER,
+  },
+  equipment: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "Equipment required"],
+    ref: RESOURCE.EQUIPMENT,
+  },
+  date_missing: {
+    type: Date,
+    required: [true, "Date missing required"],
+  },
+  date_found: {
+    type: Date,
+    required: false,
+  },
+  quantity_missing: {
+    type: Number,
+    required: [true, "Please input missing quantity"],
+  },
+  damage_quantity: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  quantity_found: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  status: {
+    type: String,
+    enum: ["Missing", "Damage", "Partially Found", "Found"],
+    default: "Missing",
+  },
+  input_qty: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model(RESOURCE.REPORT, reportSchema);
