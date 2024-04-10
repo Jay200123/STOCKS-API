f3ac2143749167721ab0eee82c314d2b8167121b
Jay200123 <kenken.ayonon@gmail.com>
2024-04-10 23:13:40 +0800

    Refactor logbook model

--- models/logbook.js
+++ models/logbook.js
@@ -9,15 +9,44 @@ const LogbookSchema = new mongoose.Schema({
   },
   equipment: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      required: [true, "equipment required"],
-      ref: RESOURCE.EQUIPMENT,
+      equipment: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, "equipment required"],
+        ref: RESOURCE.EQUIPMENT,
+      },
+      borrow_quantity: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+      missing_quantity: {
+        type: Number,
+        required: false,
+        default: 0,
+      },
+      damage_quantity: {
+        type: Number,
+        required: false,
+        default: 0,
+      },
+      status: {
+        type: String,
+        enum: ["Found","Missing","Damage", "Damage & Missing"],
+        default: "Found",
+      }
     },
   ],
   date_borrowed: {
     type: Date,
     required: [true, "date borrowed required"],
     default: Date.now(),
+  },
+  time_borrowed: {
+    type: String,
+    default: function() {
+      const currentTime = new Date();
+      return currentTime.toLocaleTimeString([], { hour12: true });
+    },
   },
   date_returned: {
     type: Date,
@@ -26,7 +55,13 @@ const LogbookSchema = new mongoose.Schema({
   note: {
     type: String,
     required: false,
-  }
+  },
+  status: {
+    type: String,
+    enum: ["Borrowed", "Returned"],
+    default: "Borrowed",
+  },
 });
+
 
 module.exports = mongoose.model(RESOURCE.LOGBOOK, LogbookSchema);
