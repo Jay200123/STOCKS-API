6852cc8fe05fc39e33b9df5e5d8a035583bb5f43
Jay200123 <kenken.ayonon@gmail.com>
2024-04-17 16:34:09 +0800

    Refactor service model

--- models/service.js
+++ models/service.js
@@ -2,27 +2,27 @@ const mongoose = require("mongoose");
 const { RESOURCE } = require("../constants/index");
 
 const serviceSchema = new mongoose.Schema({
-    service_name: {
-        type: String,
-        required: [true, "Service name field required"],
-        maxLength: [60, "Service name must not exceed to 60 characters"]
+  service_name: {
+    type: String,
+    required: [true, "Service name field required"],
+    maxLength: [60, "Service name must not exceed to 60 characters"],
+  },
+  service_price: {
+    type: Number,
+    required: [true, "Service price field required"],
+    default: 0,
+  },
+  product: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Product field required"],
+      ref: RESOURCE.PRODUCT,
     },
-    service_price: {
-        type: Number,
-        required: [true, "Service price field required"],
-        default: 0,
-    },
-    product: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            required:[true, "Product field required"],
-            ref: RESOURCE.PRODUCT, 
-        }
-    ],
-    type: {
-        type: String,
-        enum:["Hair","Eyelash","Hands"]
-    }
+  ],
+  type: {
+    type: String,
+    enum: ["Hair", "Eyelash", "Hands", "Facial", "Feet"],
+  },
 });
 
-module.exports = mongoose.model(RESOURCE.SERVICE, serviceSchema)
+module.exports = mongoose.model(RESOURCE.SERVICE, serviceSchema);
